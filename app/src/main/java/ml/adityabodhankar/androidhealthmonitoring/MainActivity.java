8546f8ca68f2e31e5ba3f7fc3d0f6cb10d48d5ed
Aditya Bodhankar <adityaabodhankar@gmail.com>
2022-01-20 19:09:25 +0530

    initial commit

---
+++ app/src/main/java/ml/adityabodhankar/androidhealthmonitoring/MainActivity.java
@@ -0,0 +1,14 @@
+package ml.adityabodhankar.androidhealthmonitoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
