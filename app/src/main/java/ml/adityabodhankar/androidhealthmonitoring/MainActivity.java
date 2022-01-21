cc710377fa64d3768882b160364d055956f1ebb1
Aditya Bodhankar <adityaabodhankar@gmail.com>
2022-01-21 17:46:13 +0530

    sign up user

--- app/src/main/java/ml/adityabodhankar/androidhealthmonitoring/MainActivity.java
+++ app/src/main/java/ml/adityabodhankar/androidhealthmonitoring/MainActivity.java
@@ -1,21 +1,17 @@
 package ml.adityabodhankar.androidhealthmonitoring;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
@@ -25,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText emailEditText, passwordEditText;
 //    private TextView forgetPassBtn;
     private FirebaseAuth auth;
+    private ProgressBar loginProgress;
+    private Button loginBtn;
+    private TextView signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +35,29 @@ public class MainActivity extends AppCompatActivity {
         if(auth.getCurrentUser()!=null){
             if(auth.getCurrentUser().isEmailVerified()){
                 //nav to homepage
+                startActivity(new Intent(this, HomeActivity.class));
             }else{
                 //nav to email verification link
+                startActivity(new Intent(this, VerificationActivity.class));
             }
+            finish();
+            return;
         }
 
         //initialize locals
-        Button loginBtn = findViewById(R.id.login_button);
+        loginBtn = findViewById(R.id.login_button);
         emailEditText = findViewById(R.id.login_email);
         passwordEditText = findViewById(R.id.login_password);
+        loginProgress = findViewById(R.id.login_progress);
+        signUpBtn = findViewById(R.id.sign_up_nav);
 //        forgetPassBtn = findViewById(R.id.forget_password_btn);
 
         //listener for login btn
         loginBtn.setOnClickListener(view -> loginUser());
+        signUpBtn.setOnClickListener(view -> {
+            startActivity(new Intent(this, SignUpActivity.class));
+            finish();
+        });
     }
 
     private void loginUser(){
@@ -57,16 +66,24 @@ public class MainActivity extends AppCompatActivity {
         if(!isDataValid(email, password)){
             return;
         }
+        loginProgress.setVisibility(View.VISIBLE);
+        loginBtn.setVisibility(View.GONE);
         //sign in user
         auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
             if(Objects.requireNonNull(authResult.getUser()).isEmailVerified()){
-                //email verified. nav to homepage
+                //nav to homepage
+                startActivity(new Intent(this, HomeActivity.class));
             }else{
-                //email not verified send email verification link.
                 //nav to email verification link
+                startActivity(new Intent(this, VerificationActivity.class));
             }
+            loginProgress.setVisibility(View.GONE);
+            loginBtn.setVisibility(View.VISIBLE);
+            finish();
         }).addOnFailureListener(e -> {
             Toast.makeText(getApplicationContext(), "Login Failed. ERR :- "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            loginProgress.setVisibility(View.GONE);
+            loginBtn.setVisibility(View.VISIBLE);
         });
     }
 
@@ -84,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         if(password.length()<6){
-            passwordEditText.setError("Minimum password length is 6");
+            passwordEditText.setError("Minimum password length is 6.");
             return false;
         }
         return true;
---
+++ app/src/main/java/ml/adityabodhankar/androidhealthmonitoring/SignUpActivity.java
@@ -0,0 +1,118 @@
+package ml.adityabodhankar.androidhealthmonitoring;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText, cPasswordEditText;
+
+    private FirebaseAuth auth;
+    private ProgressBar loginProgress;
+    private Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        //initialize firebase
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null){
+            if(auth.getCurrentUser().isEmailVerified()){
+                //nav to homepage
+                startActivity(new Intent(this, HomeActivity.class));
+            }else{
+                //nav to email verification link
+                startActivity(new Intent(this, VerificationActivity.class));
+            }
+            finish();
+            return;
+        }
+
+        //initialize locals
+        emailEditText = findViewById(R.id.signup_email);
+        passwordEditText = findViewById(R.id.signup_password);
+        cPasswordEditText = findViewById(R.id.c_signup_password);
+        loginProgress = findViewById(R.id.signup_progress);
+        signUpBtn = findViewById(R.id.signup_button);
+        TextView loginBtn = findViewById(R.id.login_nav);
+
+        signUpBtn.setOnClickListener(v -> signUp());
+        loginBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+    }
+
+    private void signUp() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String cPassword = cPasswordEditText.getText().toString();
+        if(!isDataValid(email, password, cPassword)){
+            return;
+        }
+        loginProgress.setVisibility(View.VISIBLE);
+        signUpBtn.setVisibility(View.GONE);
+        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
+            Toast.makeText(getApplicationContext(), "User Created Successfully", Toast.LENGTH_SHORT).show();
+            loginProgress.setVisibility(View.GONE);
+            signUpBtn.setVisibility(View.VISIBLE);
+            //go to verify page
+            startActivity(new Intent(getApplicationContext(), VerificationActivity.class));
+            finish();
+        }).addOnFailureListener(e -> {
+            Toast.makeText(getApplicationContext(), "User Creation Error. ERR :- "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            loginProgress.setVisibility(View.GONE);
+            signUpBtn.setVisibility(View.VISIBLE);
+        });
+    }
+
+    private boolean isDataValid(String email, String password, String cPassword) {
+        if(email.length()==0){
+            emailEditText.setError("Field Required");
+            return false;
+        }
+        if(password.length()==0){
+            passwordEditText.setError("Field Required");
+            return false;
+        }
+        if(cPassword.length()==0){
+            cPasswordEditText.setError("Field Required");
+            return false;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Enter Valid Email.");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Minimum password length is 6.");
+            return false;
+        }
+        if(cPassword.length()<6){
+            cPasswordEditText.setError("Minimum password length is 6.");
+            return false;
+        }
+        if(password.compareTo(cPassword)!=0){
+            passwordEditText.setError("Both Passwords not match.");
+            cPasswordEditText.setError("Both Passwords not match");
+            return false;
+        }
+        return true;
+    }
+}
